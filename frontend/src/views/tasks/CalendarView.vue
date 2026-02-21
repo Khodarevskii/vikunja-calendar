ea7415ec13c4be6830aac520a9ae87b24f88004b
«khodarevskii» <79624036110@yandex.ru>
2026-02-21 23:43:27 +0300

    поправил визуал

--- frontend/src/views/tasks/CalendarView.vue
+++ frontend/src/views/tasks/CalendarView.vue
@@ -561,11 +561,15 @@ onMounted(async () => {
 }
 ::v-deep(.fc-theme-standard td, .fc-theme-standard th){
 	border:1px solid var(--border) !important;
+}	
+::v-deep(.calendar-wrapper .fc-day-today .fc-daygrid-day-frame){
+	background: var(--fc-today-bg-color);
 }
-
 ::v-deep(.fc-col-header-cell.fc-day){
 	border:1px solid var(--border) !important;
 }
+
+
 
 :deep(.fc-theme-standard .fc-scrollgrid){
 	border:1px solid var(--border);
@@ -584,6 +588,11 @@ onMounted(async () => {
 	background-color: var(--modal-color) !important;
 	border-color: var(--border) !important;
 }
+
+:deep(.fc-theme-standard .fc-list-day-cushion){
+	background: var(--white);
+	color:var(--input-color)
+}
 :deep(.modal-card-foot){
 	background-color: var(--modal-color) !important;
 	border-color: var(--border) !important;
@@ -595,7 +604,23 @@ onMounted(async () => {
 	filter: invert(var(--filter));
 }
 
+:deep(.fc .fc-list-event:hover td ){
+    background-color: var(--white);
+	opacity: 0.6;
+}
+:deep(.fc-theme-standard td, .fc-theme-standard th){
+	border:1px solid var(--border) !important;
+}
+:deep(.calendar-wrapper th){
+	border:1px solid var(--border) !important;
+}	
+:deep(.calendar-wrapper .fc-list-event-graphic){
+	padding: 10px;
+}
 
+:deep(.calendar-wrapper .fc-theme-standard .fc-list){
+	border:1px solid var(--border) !important;
+}
 
 :deep(.fc-button) {
 	background-color: var(--primary) !important;
@@ -615,6 +640,10 @@ onMounted(async () => {
 
 :deep(.fc-daygrid-event) {
 	cursor: pointer;
+}
+
+:deep(.fc-timegrid-axis){
+	border:none
 }
 
 :deep(.fc-highlight) {
